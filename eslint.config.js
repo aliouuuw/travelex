639c2158f70f3b31c395dd89593ad725fd723f26
aliouuuw <wadealiou00@gmail.com>
2025-06-24 16:19:00 +0000

    Refactor project configuration and dependencies; enhance ESLint setup and add theming support. Updated package.json with new dependencies including Radix UI and Supabase, improved ESLint configuration for better React and TypeScript support, and established CSS variables for theming in index.css.

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,45 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+import tanstackQuery from "@tanstack/eslint-plugin-query";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
 
-export default tseslint.config([
-  globalIgnores(['dist']),
+
+export default tseslint.config(
   {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
+    ignores: ["dist", "node_modules", "eslint.config.js"],
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    ...pluginReactConfig,
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+      ...pluginReactConfig.languageOptions,
+      parser: tseslint.parser,
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+      },
+      globals: { ...globals.browser, ...globals.es2020 },
+    },
+    plugins: {
+      "react-refresh": reactRefresh,
+      "react-hooks": reactHooks,
+      "@tanstack/query": tanstackQuery,
+    },
+    rules: {
+      ...tanstackQuery.configs.recommended.rules,
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
     },
   },
-])
+  ...tseslint.configs.recommended,
+);
