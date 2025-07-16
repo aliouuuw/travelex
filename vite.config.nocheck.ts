be92cd511a68d710e71b175b0559fd87b01cd491
aliouuuw <wadealiou00@gmail.com>
2025-07-16 01:26:02 +0000

    Fixed hot reload issue

--- vite.config.nocheck.ts
+++ vite.config.nocheck.ts
@@ -2,19 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
-import checker from "vite-plugin-checker";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    checker({
-      typescript: true,
-      overlay: {
-        initialIsOpen: false,
-      },
-    }),
   ],
   resolve: {
     alias: {
